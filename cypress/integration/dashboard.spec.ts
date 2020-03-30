1107f26b1d962b5a64b6c0dac2de735f5f77045f
kevin leary <kevin.leary@navy.mil>
2020-03-30 14:21:38 -0400

    using cy.title

--- cypress/integration/dashboard.spec.ts
+++ cypress/integration/dashboard.spec.ts
@@ -9,10 +9,15 @@ describe('dashboard', () => {                                       //grouping h
 
     //Homework 1
     it(`should have text "Top Heroes" in the right place and "Hero Search" in the right place`, () =>{
-        cy.visit('http://localhost:4200')
+        cy.visit('http://localhost:4200')                           //ng routing for url no need for 4200
         cy.get('h3').should('contain.text', 'Top Heroes')
         cy.get('h4').should('contain.text', 'Hero Search')
     })
-
+    //End Homework 1 
+    
+    it(`has the title of "Tour of Heroes"`, () => {                 //cypress is async... ran in a queue, executed immediately
+        cy.visit('http://localhost:4200')
+        cy.title().should('eq', 'Tour of Heroes')
+    })
 
 })
