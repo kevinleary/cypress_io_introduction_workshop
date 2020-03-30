69bc5b96ead0e714579ed1a89471d0ec6536ebae
kevin leary <kevin.leary@navy.mil>
2020-03-30 13:54:07 -0400

    Homework 1: Should have text in headers

--- cypress/integration/dashboard.spec.ts
+++ cypress/integration/dashboard.spec.ts
@@ -4,7 +4,14 @@ describe('dashboard', () => {                                       //grouping h
     it(`has an h1 with "Tour of Heroes"`, () => {                   //actual code here
         //cypress doesn't care what url... you have to give it one!
         cy.visit('http://localhost:4200/dashboard')                 //cy. for almost all cypress functions 
-        cy.get('h1').should('have.text', 'Tour of Heroes')
+        cy.get('h1').contains('Tour')
+    })
+
+    //Homework 1
+    it(`should have text "Top Heroes" in the right place and "Hero Search" in the right place`, () =>{
+        cy.visit('http://localhost:4200')
+        cy.get('h3').should('contain.text', 'Top Heroes')
+        cy.get('h4').should('contain.text', 'Hero Search')
     })
 
 
